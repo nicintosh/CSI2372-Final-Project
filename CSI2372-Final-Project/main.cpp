47e436441fd971ae3828cd8004cd075be9da79d0
nicintosh <nickburgel@me.com>
2014-11-30 19:57:41 -0500

    Initial Commit

---
+++ CSI2372-Final-Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSI2372-Final-Project
+//
+//  Created by Nick Burgel on 2014-11-30.
+//  Copyright (c) 2014 Nick Burgel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
